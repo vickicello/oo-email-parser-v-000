9ca314db25326b34b4f873ee8885a3cb64e839d0
Vicki Hamm <vicki.cello@gmail.com>
2018-06-26 14:23:09 +0000

    automatically backed up by learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -8,7 +8,7 @@ class EmailParser
   end
 
   def parse
-    new_list = @emails.split(/\s*,\s*/)
+    new_list = @emails.split(/\s*, | \s*/)
     parsed_list = new_list.delete_if { |email| email == "" }
     parsed_list.uniq
   end
